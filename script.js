d76df2159774ee317d2eada8fc0de77005aa648c
Bharath552-bit <patthembharath32@gmail.com>
2026-01-15 21:15:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 // your code here
+const form=document.querySelector("form")
+const heading=document.querySelector("#url")
+const name=document.querySelector("#name").value
+const year=document.querySelector("#year").value
+form.addEventListener('submit',(e)=>{
+	heading.innerText=`https://localhost:8080/?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`
+})
